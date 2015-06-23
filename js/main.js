438df28781cb4b2a6b21a3d0f44c7c467a8c47f6
warlyware <danward@gmail.com>
2015-06-22 17:16:30 -0700

    Moving tower of hanoi game to personal repo

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var $selectedBlock, selectedBlockVal, towerValue, blockIsSelected = false;
+
+$(document).ready(function() {
+  $('.win-text').hide();
+  $('.tower-wrapper').on('click', function(ev) {
+    $tower = $(this);
+    if (($tower.children().length < 1) && !blockIsSelected) { // if tower empty and no block selected
+      // do nothing
+    } else if (($tower.children().length < 1) && blockIsSelected) { // else tower empty and block selected
+      $(this).append($selectedBlock)
+      $selectedBlock.removeClass('selected');
+      blockIsSelected = false;
+    } else if (($tower.children().length > 0) && blockIsSelected) { // else see if tower has allowed move
+      selectedBlockVal = $selectedBlock.attr('data-value');
+      selectedTowerVal = $tower.children().first().attr('data-value');
+      $selectedBlock.toggleClass('selected');
+      if (selectedBlockVal < selectedTowerVal) {
+        $selectedBlock.prependTo($tower).removeClass('selected');
+      }
+      blockIsSelected = false;
+      $selectedBlock = undefined;
+    } else if (!blockIsSelected){ // otherwise grab block
+      $selectedBlock = $(this).children().first(); // set selected block
+      blockIsSelected = true;
+      $selectedBlock.addClass('selected')
+    } else if ($tower.children().first().attr('data-value') === $selectedBlock.attr('data-value')) {
+      $tower.children().first().toggleClass('selected');
+    }
+
+    if ($('.tower-3').children().length === 4) {
+      $('.win-text').show();
+      console.log('win');
+    }    
+  });
+});
